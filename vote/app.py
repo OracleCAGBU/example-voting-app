4a5ae87a2bbaf6d3d0dbec5b795cf29b99ec84e9
Debapriya Roy <500578@btc.com.bh>
2026-06-12 18:04:58 +0530

    Added more env var

--- vote/app.py
+++ vote/app.py
@@ -18,8 +18,8 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        redis_host = os.getenv("REDIS_HOST", "redis")
-        redis_port = int(os.getenv("REDIS_PORT", "6379"))
+        redis_host = os.getenv("REDIS_SERVICE_HOST", "redis")
+        redis_port = int(os.getenv("REDIS_SERVICE_PORT_REDIS", "6379"))
         redis_password = os.getenv("REDIS_PASSWORD")
 
         g.redis = Redis(
